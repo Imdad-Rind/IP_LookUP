6b9fb898210a02adff7ab350b92281fcc1a8ab32
FaisaljanBaloch <faisaljanbaloch@gmail.com>
2023-10-15 10:14:49 +0500

    Fixes typos

--- main.py
+++ main.py
@@ -7,35 +7,35 @@ import time
 import ip_works
 
 
-with yaspin(spinners.Spinners.line,text=" IP LookUP loading ... ",color="yellow"):
+with yaspin(spinners.Spinners.line, text=" IP LookUP loading ... ", color="yellow"):
     time.sleep(1.2)
 
 
 def main(
-        IP_Adress: str = typer.Option(..., prompt=True)
+        IP_Address: str = typer.Option(..., prompt=True)
 ):
 
-    options = [inquirer.List("ip_info_choices", message="what type of information you want ??",
-                             choices=["IP Class", "Net & Host InFo", "All"])]
+    options = [inquirer.List("ip_info_choices", message="What type of information you want?",
+                             choices=["IP Class", "Net & Host Info", "All"])]
 
     choices = inquirer.prompt(options)
     if choices["ip_info_choices"] == "IP Class":
-        print(f" IP Address {IP_Adress} is in Class {ip_works.ip_Class_Info(IP_Adress)}")
-    if choices["ip_info_choices"] == "Net & Host InFo":
-        print(ip_works.ip_host_and_net_info(ip_works.ip_Class_Info(IP_Adress),IP_Adress))
+        print(f" IP Address {IP_Address} is in Class {ip_works.ip_Class_Info(IP_Address)}")
+    if choices["ip_info_choices"] == "Net & Host Info":
+        print(ip_works.ip_host_and_net_info(ip_works.ip_Class_Info(IP_Address), IP_Address))
     if choices["ip_info_choices"] == "All":
-        print("[bright_blue]Feature[/bright_blue] : Comeing soon ...")
+        print("[bright_blue]Feature[/bright_blue] : Coming soon ...")
 
-    print("\n\n❤️ [bright_green]Thank You for using IP LookUP[bright_green] \n More Featurs Coming soon\n\n\n")
+    print("\n\n❤️ [bright_green] Thank You for using IP LookUP[bright_green] \n More Featurs Coming soon\n\n\n")
 
     typer.pause()
 
 
 if __name__ == '__main__':
-    f = pyfiglet.figlet_format("  \"  IP Look UP  \"",font="slant")
+    f = pyfiglet.figlet_format("  \"  IP Look UP  \"", font="slant")
     print(f)
-    print("😻[bold yellow] Wellcome to IP LookUp... [/bold yellow]\n\n\n")
-    print(" Enter your [bright_cyan]ID Address[/bright_cyan] to get [cyan]info[/cyan] about [red]:[/red] \n\n like this ..."
+    print("😻[bold yellow] Welcome to IP LookUp... [/bold yellow]\n\n\n")
+    print(" Enter your [bright_cyan]IP Address[/bright_cyan] to get [cyan]info[/cyan] about [red]:[/red] \n\n like this ..."
           " \n\n 👉 192.168.18.1 \nor\n 👉 192.168.18.1/25\n\n\n")
     typer.run(main)
 
